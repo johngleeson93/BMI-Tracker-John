43668f3254436c850c13e639952c9e8a24934003
John Gleeson <59605829+johngleeson93@users.noreply.github.com>
2020-06-07 09:26:18 +0100

    Math.round(bmi)

---
+++ Utility.java
@@ -0,0 +1,37 @@
+package controllers;
+
+import models.Assessment;
+import models.Member;
+import play.db.jpa.Model;
+
+public class Utility extends Model {
+    public double bmi;
+    public String Category;
+
+    public Utility() {
+    }
+
+    public static double calculateBMI(Member member, Assessment assessmentList) {
+        double bmi;
+            bmi = ((assessmentList.getWeight()) / ((member.getHeight() / 100) * (member.getHeight() / 100)));
+        return Math.round(bmi);
+    }
+
+    public static String getCategory(double bmi) {
+        if (bmi > 30) {
+            return "You" +
+                    " are" +
+                    " obese.";
+        } else if ((bmi > 24.99) && (bmi < 30)) {
+            return "You're" +
+                    " overweight.";
+        } else if ((bmi > 18.5) && (bmi < 24.99)) {
+            return "You're" +
+                    " normal" +
+                    "weight.";
+        } else
+            return "You're" +
+                    " under" +
+                    "weight.";
+    }
+}
